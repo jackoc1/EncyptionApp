d344a3d263df0baa0204616c38aa0faaa074b573
jackoc1 <jackaoconnor1@gmail.com>
2022-09-27 16:20:38 +0100

    Public/private key generation

--- src/crypto/keys.js
+++ src/crypto/keys.js
@@ -1,13 +1,43 @@
 const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
 
-const generateKeys = () => {
-
+const writeKeysSync = (dir) => {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+          type: "pkcs1",
+          format: "pem",
+        },
+        privateKeyEncoding: {
+          type: "pkcs1",
+          format: "pem",
+        },
+      });
+      
+      fs.writeFileSync(path.join(dir, 'public.pem'), publicKey);
+      fs.writeFileSync(path.join(dir, 'private.pem'), publicKey);
 };
 
-const readKeys = () => {
+const readKeysSync = (dir) => {
+    try {
+        const publicKeyPem = fs.readFileSync(path.join(dir, 'public.pem'));
+        const privateKeyPem = fs.readFileSync(path.join(dir, 'private.pem'));
+    } catch(e) {
+        if (e instanceof ENOENT) {
+            return undefined;
+        } else {
+            throw e;
+        }
+    }
 
+    const publicKey = crypto.createPublicKey(publicKeyPem);
+    const privateKey = crypto.createPrivateKey(privateKeyPem);
+
+    return { publicKey, privateKey };
 };
 
 module.exports = {
-    generateKeys
+    writeKeysSync,
+    readKeysSync
 };
