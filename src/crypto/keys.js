12ffd3165ae70fa276b5824a60656c6114b4a66b
jackoc1 <jackaoconnor1@gmail.com>
2022-09-27 17:07:26 +0100

    Cipher keys added

--- src/crypto/keys.js
+++ src/crypto/keys.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const writeKeysSync = (dir) => {
-    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {  // Assymetric encryption -> signatures
         modulusLength: 2048,
         publicKeyEncoding: {
           type: "pkcs1",
@@ -14,12 +14,20 @@ const writeKeysSync = (dir) => {
           format: "pem",
         },
       });
+
+      const secretKey = crypto.randomBytes(32);
+      const initVector = crypto.randomBytes(16);
+      const cipher_keys = JSON.stringify({
+        secretKey,
+        initVector
+      });
       
       fs.writeFileSync(path.join(dir, 'public.pem'), publicKey);
       fs.writeFileSync(path.join(dir, 'private.pem'), publicKey);
+      fs.writeFileSync(path.join(dir, 'cipher_keys.json'), cipher_keys);
 };
 
-const readKeysSync = (dir) => {
+const readSignatureKeysSync = (dir) => {
     try {
         const publicKeyPem = fs.readFileSync(path.join(dir, 'public.pem'));
         const privateKeyPem = fs.readFileSync(path.join(dir, 'private.pem'));
@@ -37,7 +45,25 @@ const readKeysSync = (dir) => {
     return { publicKey, privateKey };
 };
 
+const readCipherKeysSync = (dir) => {
+    try {
+        const cipher_keys = JSON.parse(fs.readFileSync(path.join(dir, 'cipher_keys.json')));
+    } catch(e) {
+        if (e instanceof ENOENT) {
+            return undefined;
+        } else {
+            throw e;
+        }
+    }
+
+    const secretKey = Buffer.from(cipher_keys.secretKey, 'hex');
+    const initVector = Buffer.from(cipher_keys.initVector, 'hex');
+
+    return { secretKey, initVector };
+}
+
 module.exports = {
     writeKeysSync,
-    readKeysSync
+    readSignatureKeysSync,
+    readCipherKeysSync
 };
