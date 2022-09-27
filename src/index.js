12fda98929be9fa124c09ef51a30fe07216805e0
jackoc1 <jackaoconnor1@gmail.com>
2022-09-27 18:43:15 +0100

    Write keys to disk if keys don't exist on startup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const app = require('./app');
-const port = process.env.PORT ;
+const path = require('path');
+const { writeKeysSync } = require('./crypto/keys');
+
+const port = process.env.PORT;
+
+if (!fs.existsSync(path.join('../keys/', process.env.KEYS_DIR))) {
+    fs.mkdirSync(path.join('../keys/', process.env.KEYS_DIR));
+    writeKeysSync(path.join('../keys/', process.env.KEYS_DIR));
+}
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
